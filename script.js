8e3f612bdbeb1648912b5208b3a6b044e56c9cfd
hitesh <bhatnagerhitesh@gmail.com>
2026-06-08 13:28:45 +0530

    new

--- script.js
+++ script.js
@@ -1,41 +1,74 @@
 let title = document.querySelector(".title-box");
 let addButton = document.querySelector(".add-btn");
 let main = document.querySelector("main");
+let heading = document.querySelector("h1");
+let notePage = document.querySelector(".note-page");
+let search = document.querySelector(".search-box");
+let searchButton = document.querySelector(".search-btn");
+let home = document.querySelector(".home");
+
+let currentId = null;
+
+
+title.focus();
+
+heading.addEventListener("click", () => {
+  localStorage.removeItem("dataSave");
+});
 
 // forming data sturcture
 let data = JSON.parse(localStorage.getItem("dataSave")) || [];
 
 // saving data in local storage
 function localSave(sav) {
-    localStorage.setItem("dataSave", JSON.stringify(sav));
+  localStorage.setItem("dataSave", JSON.stringify(sav));
 }
 
+let colorsArray = [
+  "#eb6856", // soft yellow
+  "#f3d24c",
+  "#498be0", // soft blue
+  "#bfdbfe",
+  "#8ce2aa", // soft green
+  "#bbf7d0",
+  "#ec78ba", // soft pink
+  "#e794c3",
+  "#141025", // soft purple
+  "#9a86f1"
+]
+
 addButton.addEventListener("click", (e) => {
-    if (title.value !== "") {
-        e.preventDefault();
-        data.push({
-            id: Date.now(),
-            title: title.value,
-            date: new Date().toLocaleDateString(),
-            pined: false,
-        });
-        render(data);
-        localSave(data)
-        title.value = "";
-    }
+  if (title.value !== "") {
+    e.preventDefault();
+    data.push({
+      id: Date.now(),
+      title: title.value,
+      date: new Date().toLocaleDateString(),
+      pinned: false,
+      content: "",
+      color: "#ffffff"
+    });
+    render(data);
+    localSave(data);
+    title.value = "";
+  }
 });
 
 function render(str) {
-    main.innerHTML = str.map((obj,i) => `
-    <div class="notes-box" data-id="${obj.id}">
+  main.innerHTML = str
+    .map(
+      (obj, i) => `
+    <div class="notes-box" data-id="${obj.id}" style = "background: ${obj.color}">
         <div class="box">
-          <div class="date">${obj.date}</div>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pin-off-icon lucide-pin-off hidden"><path d="M12 17v5"/><path d="M15 9.34V7a1 1 0 0 1 1-1 2 2 0 0 0 0-4H7.89"/><path d="m2 2 20 20"/><path d="M9 9v1.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h11"/></svg>
-        </div>
+          <div class="date">${obj.date}</div>       
+             <div class = "unpin" data-id="${obj.id}">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pin-off-icon lucide-pin-off ${obj.pinned ? "" : "hidden"}"><path d="M12 17v5"/><path d="M15 9.34V7a1 1 0 0 1 1-1 2 2 0 0 0 0-4H7.89"/><path d="m2 2 20 20"/><path d="M9 9v1.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h11"/></svg>
+        </div>     
+               </div>
         <div class="notes-title">
           <div class="title" data-id="${i}">${obj.title}</div>
           <div class="option-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            <svg xmlns="http:/ /www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
               class="lucide lucide-ellipsis-vertical-icon lucide-ellipsis-vertical">
               <circle cx="12" cy="12" r="1" />
@@ -56,7 +89,7 @@ function render(str) {
               <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
             </svg>
           </div>
-          <div class="pin">
+          <div class="pin" data-id="${obj.id}">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
               class="lucide lucide-pin-icon lucide-pin">
@@ -65,7 +98,7 @@ function render(str) {
                 d="M9 10.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V7a1 1 0 0 1 1-1 2 2 0 0 0 0-4H8a2 2 0 0 0 0 4 1 1 0 0 1 1 1z" />
             </svg>
           </div>
-          <div class="edit">
+          <div class="edit" data-id="${i}">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
               class="lucide lucide-pencil-icon lucide-pencil">
@@ -74,7 +107,7 @@ function render(str) {
               <path d="m15 5 4 4" />
             </svg>
           </div>
-          <div class="color">
+          <div class="color" data-id="${obj.id}">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
               class="lucide lucide-palette-icon lucide-palette">
@@ -88,52 +121,185 @@ function render(str) {
           </div>
         </div>
       </div>
-  </div>`
-    ).join("");
+  </div>`,
+    )
+    .join("");
 }
 
-main.addEventListener('click', (e) => {
-    let mainBox = e.target.closest(".notes-box")
-    let settings = e.target.closest(".option-icon")
-    let delButton = e.target.closest(".trash")
-    let pin = e.target.closest(".pin")
-    let unpin = e.target.closest(".lucide-pin-off")
-    let edit = e.target.closest(".edit")
+main.addEventListener("click", (e) => {
+  let mainBox = e.target.closest(".notes-box");
+  let settings = e.target.closest(".option-icon");
+  let delButton = e.target.closest(".trash");
+  let pin = e.target.closest(".pin");
+  let unpin = e.target.closest(".unpin");
+  let edit = e.target.closest(".edit");
+  let paint = e.target.closest(".color");
+  let title = e.target.closest(".title");
+  let home = e.target.closest(".home");
 
-    if (settings) {
-        let box = settings.closest(".notes-box")
-        let menu = box.querySelector(".menu")
-        menu.classList.toggle("hidden")
-        e.target.closest("main").addEventListener('click', () => {
-            menu.classList.add("hidden")
-        })
-    } else if (delButton) {
-        let index = mainBox.dataset.id
-        let delData = data.filter((t) => !(index == t.id))
-        render(delData)
-        localSave(delData)
-    } else if (pin) {
-        let box = pin.closest(".notes-box")
-        let newunpin = box.querySelector(".lucide-pin-off")
-        newunpin.classList.remove("hidden")
-    } else if (unpin) {
-        unpin.classList.add("hidden")
-    }else if (edit){
-        let box = edit.closest(".notes-box")
-        let input = box.querySelector(".title")
-        let index2 = input.dataset.id
-        input.innerHTML = `<input type="text" class="new-input" value="${input.innerText}" name="input">`
+  document.querySelectorAll(".menu").forEach((a) => {
+    a.classList.add("hidden");
+  });
 
-        let newInput = box.querySelector(".new-input")
-        newInput.focus()
-        input.addEventListener('keydown', (e) => {
-            if(e.key === "Enter"){
-                data[index2].title === newInput.value
-                render(data)
-                localSave(data)
-            }
-        })
+  if (settings) {
+    let box = settings.closest(".notes-box");
+    let menu = box.querySelector(".menu");
+    menu.classList.remove("hidden");
+  } else if (delButton) {
+    deleteFuntion(mainBox)
+  } else if (pin) {
+    let index = pin.dataset.id;
+
+    let note = data.find((t) => t.id == index);
+    note.pinned = !note.pinned
+
+    render(data);
+    localSave(data);
+  } else if (unpin) {
+    let index = unpin.dataset.id;
+
+    let note = data.find((t) => t.id == index);
+    note.pinned = !note.pinned
+
+    render(data);
+    localSave(data);
+  } else if (edit) {
+    let box = edit.closest(".notes-box");
+    let input = box.querySelector(".title");
+    let index = input.dataset.id;
+    input.innerHTML = `<input type="text" class="new-input" value="${input.innerText}" name="input">`;
+
+    let newInput = box.querySelector(".new-input");
+    newInput.focus();
+    input.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        data[index].title = newInput.value;
+        render(data);
+        localSave(data);
+      }
+    });
+  } else if (paint) {
+    colorFunction(paint)
+  } else if (
+    mainBox &&
+    !settings &&
+    !pin &&
+    !unpin &&
+    !paint &&
+    !edit &&
+    !delButton
+  ) {
+    let id = mainBox.dataset.id;
+    openPage(id);
+    currentId = id
+  }
+});
+
+function colorFunction(paint) {
+  let id = paint.dataset.id
+  let col = data.find((t) => t.id == id)
+
+  let num = Math.floor(Math.random() * colorsArray.length)
+  col.color = colorsArray[num]
+  render(data)
+  localSave(data)
+}
+
+function deleteFuntion(mainBox) {
+  let id = mainBox.dataset.id;
+  let index = data.findIndex((t) => t.id == id);
+  data.splice(index, 1)
+  render(data);
+  localSave(data);
+}
+
+searchButton.addEventListener("click", (e) => {
+  searchFun(e);
+});
+
+function searchFun(e) {
+  e.preventDefault();
+  let searchTitle = search.value.toLowerCase();
+  let searchData = data.filter((t) =>
+    t.title.toLowerCase().includes(searchTitle),
+  );
+
+  render(searchData);
+  search.value = "";
+}
+
+home.addEventListener("click", (e) => {
+  if (
+    search.value !== "" &&
+    !e.target.closest(".search-btn") &&
+    !e.target.closest(".search-box")
+  ) {
+    console.log("hiteshsd");
+    render(data);
+  }
+});
+
+render(data);
+
+let page2 = document.querySelector(".page-2");
+let noteTitle = document.querySelector(".notes-title span");
+let noteContent = document.querySelector("#notes");
+let noteDate = document.querySelector(".date span");
+let homeBtn = document.querySelector(".home-btn");
+let pinNotes = document.querySelector(".pin-notes")
+let delButton = document.querySelector(".del")
+let colorNote = document.querySelector(".color-note")
+
+function homeFun() {
+  home.classList.remove("hidden");
+  page2.classList.add("hidden");
+}
+
+homeBtn.addEventListener("click", () => {
+  homeFun()
+});
+
+function openPage(id) {
+  let note = data.find((n) => n.id == id);
+
+  saveContent(note);
+
+  home.classList.add("hidden");
+  page2.classList.remove("hidden");
+
+  noteTitle.innerText = note.title;
+  noteDate.innerText = note.date;
+  noteContent.value = note.content || "";
+
+  setTimeout(() => {
+    noteContent.focus();
+  }, 100)
+}
+
+function saveContent(note) {
+  noteContent.addEventListener("input", () => {
+    if (noteContent.value != "") {
+      note.content = noteContent.value;
+      render(data);
+      localSave(data);
     }
+  });
+}
+
+
+pinNotes.addEventListener('click', () => {
+  let note = data.find(n => n.id == currentId)
+
+  note.pinned = !note.pinned   // 🔥 toggle
+
+  render(data)
+  localSave(data)
 })
 
-render(data)
+delButton.addEventListener('click', () => {
+  let index = data.findIndex((t) => t.id == currentId)
+  data.splice(index, 1)
+  render(data)
+  localSave(data)
+  homeFun()
+})
